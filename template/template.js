8358927b18806267c958172122ca952683dc406b
kele <damian.bogel@gmail.com>
2015-01-14 23:21:06 +0100

    Heavy WIP on template

--- template/template.js
+++ template/template.js
@@ -1,10 +1,4 @@
-$(document).ready(function(){
-  var canvas = document.getElementById("bgCanvas");
-  var ctx = canvas.getContext("2d");
-  ctx.fillStyle = "red";
-  ctx.fillRect(0, 0, 2, 1);
-  var data = canvas.toDataURL();
-  
-  $('.message').css('background-image', 'url(' + data + ')');
-  $(".message").css('background-repeat', 'repeat-y');
+$(document).ready(function () {
+    var xxx = document.getElementById("xxx");
+    console.log(xxx.offsetWidth); 
 });
