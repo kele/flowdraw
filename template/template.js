47fe664f6e1349630f7040da5172f7c6b97b8a01
kele <damian.bogel@gmail.com>
2015-01-16 23:07:03 +0100

    Arrow issue not resolved yet.

--- template/template.js
+++ template/template.js
@@ -1,21 +1,47 @@
+
+var redraw = function (canvas)
+{ 
+  var height = canvas.clientHeight;
+  var width = canvas.clientWidth;
+  
+  //console.log(height + " " + width);
+  
+  var ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.clearRect(0, 0, width, height);
+  ctx.moveTo(0, 2);
+  ctx.lineTo(width - 1, 2);
+  ctx.moveTo(width - 1, 2);
+  ctx.lineTo(width - 7, 0);
+  ctx.moveTo(width - 1, 2);
+  ctx.lineTo(width - 7, 4);
+  ctx.strokeStyle = "red 2px";
+  ctx.stroke();
+}
+  
+var redrawAll = function()
+{
+  var canvases = document.getElementsByTagName("canvas");
+  console.log(canvases);
+  for (var i = 0; i < canvases.length; i++)
+    redraw(canvases[i]);
+}
+
 $(document).ready(function () {
   var messages = document.getElementsByClassName("message");
   
   for (var i = 0; i < messages.length; i++)
   {
-      messages[i].style.color = "green";
-      console.log(messages[i].offsetHeight);
-      console.log(messages[i].offsetWidth);
-      
       var canvasElement = document.createElement("canvas");
-      var width = canvasElement.width = messages[i].clientWidth;
+      canvasElement.style.width = "100%";
+      canvasElement.style.maxHeight = "5px";
+      canvasElement.height = 5;
       var height = canvasElement.height = 5;
+      var width = canvasElement.width;
       
       var br = document.createElement("br");
       messages[i].insertBefore(br, messages[i].firstChild);
       messages[i].insertBefore(canvasElement, messages[i].firstChild);
-      console.log(canvasElement);
-      canvasElement.primary_width = width;
       
       var ctx = canvasElement.getContext("2d");
       ctx.beginPath();
@@ -28,39 +54,19 @@ $(document).ready(function () {
       ctx.stroke();
   }
   
-var redraw = function (messageTd)
-{
-  console.log(messageTd);
-  var canvas = messageTd.getElementsByTagName("canvas")[0];
-  console.log(canvas);
-  
-  var height = 5;
-  var width = canvas.width = messageTd.clientWidth;
-  
-  
-  var ctx = canvas.getContext("2d");
-  ctx.beginPath();
-  ctx.moveTo(0, 2);
-  ctx.lineTo(width - 1, 2);
-  ctx.moveTo(width - 1, 2);
-  ctx.lineTo(width - 5, 0);
-  ctx.moveTo(width - 1, 2);
-  ctx.lineTo(width - 5, 4);
-  ctx.stroke();
-}
-  
+    
   $(".message").click(function (event) {
     var messageId = event.currentTarget.id;
     if (messageId == 1)
     {
-      $("#really_hidden").toggle("slow", function() { redraw(event.currentTarget); });
-      
+      $("#really_hidden").toggle("slow");
     }
     else if (messageId == 4)
     {
-      $("#really_hidden2").toggle("slow", function() { redraw(event.currentTarget); });
+      $("#really_hidden2").toggle("slow");
     }
   });
-  
-
 });
+
+
+
